4724fa3503f715eceaeab4d7a1ac38aced3df525
faten-ibrahim <faten_elssayad@yahoo.com>
2024-06-11 12:03:56 +0300

    enable csrf protection in index page

--- src/resources/views/notes/index.blade.php
+++ src/resources/views/notes/index.blade.php
@@ -2,6 +2,8 @@
 <html>
 
 <head>
+    <meta name="_token" content="{{ csrf_token() }}">
+
     <title>notes</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap5.min.css" rel="stylesheet">
